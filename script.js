ede85bd2316f9489d9f3d8bec2b379aafb702a75
Nugraha Panca Wibisana <nugrahapancawibisana@gmail.com>
2025-11-25 14:09:48 +0700

    feat: adds styling and add parse int to converter

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const celciusToFahrenheit = (c) => {
-  return c * 1.8 + 32;
+  return parseInt(c) * 1.8 + 32;
 };
 
 const celciusToKelvin = (c) => {
-  return c + 273.15;
+  return parseInt(c) + 273.15;
 };
 
 const celciusToReamur = (c) => {
-  return c * 0.8;
+  return parseInt(c) * 0.8;
 };
 
 const form = document.querySelector("form");
+
+document.body.style.padding = "50px";
+document.body.style.fontFamily = "Arial, Helvetica, sans-serif";
+document.querySelector("label").style.fontSize = "20px";
+
+form.style.display = "flex";
+form.style.flexDirection = "column";
+form.style.width = "300px";
+form.style.gap = "10px";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
