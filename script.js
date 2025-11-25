ae9313b52f04d974317850e511f9a9e6b3c7ef61
Nugraha Panca Wibisana <nugrahapancawibisana@gmail.com>
2025-11-25 13:52:19 +0700

    feat: adds celcius to kelvin converter

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ const celciusToFahrenheit = (c) => {
   return c * 1.8 + 32;
 };
 
+const celciusToKelvin = (c) => {
+  return c + 273.15;
+};
+
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(e.target.celcius.value);
 
-  document.querySelector("div p:nth-child(1)").textContent = `Celcius: ${e.target.celcius.value}`;
-  document.querySelector("div p:nth-child(2)").textContent = `Fahrenheit: ${celciusToFahrenheit(e.target.celcius.value)}`;
+  document.querySelector(
+    "div p:nth-child(1)"
+  ).textContent = `Celcius: ${e.target.celcius.value}`;
+
+  document.querySelector(
+    "div p:nth-child(2)"
+  ).textContent = `Fahrenheit: ${celciusToFahrenheit(e.target.celcius.value)}`;
+
+  document.querySelector(
+    "div p:nth-child(3)"
+  ).textContent = `Fahrenheit: ${celciusToKelvin(e.target.celcius.value)}`;
 });
